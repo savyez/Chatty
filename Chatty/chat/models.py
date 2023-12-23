4e2a017442cc94c3b34c57110f04c6330e857378
Saurabh Biswas <visnuk252@gmail.com>
2023-12-23 21:31:44 +0530

    models registrations to admin site

--- Chatty/chat/models.py
+++ Chatty/chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length = 50)
+
+    def __str__(self) -> str:
+        return self.name.__str__()
+    
+class Message(models.Model):
+    value = models.CharField(max_length=512)
+    date = models.DateTimeField(default= datetime.now, blank=True)
+    user = models.CharField(max_length=60)
+    room = models.CharField(max_length=255)
